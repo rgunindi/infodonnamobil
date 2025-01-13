111b543a5d4a8fb7fc2e14af67c93721a5977392
Ramazan G <codmyhesap@gmail.com>
2025-01-13 06:00:32 +0100

    modified:   src/main.rs

---
+++ src/component/prelude.rs
@@ -0,0 +1,12 @@
+pub use dioxus::{logger::tracing::info, prelude::*};
+
+mod server {
+    include!("../../server/server.rs");
+}
+pub use server::{get_markdown,watch_markdown};
+
+#[cfg(feature = "server")]
+pub use server::MarkdownEntry;
+
+#[cfg(feature = "server")]
+pub use tokio::time::Duration;
